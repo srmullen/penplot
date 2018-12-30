addc42a1e6d9dbcfa591c8d5373e4318a764b499
Sean Mullen <srmullen13@gmail.com>
2018-12-30 13:18:36 -0700

    hyphae fns

--- src/sketch/hyphae/hyphae.js
+++ src/sketch/hyphae/hyphae.js
@@ -1,20 +1,14 @@
-import paper, { Point, Path } from 'paper';
+import paper, { Point, Path, Group } from 'paper';
 import { A4, STRATH_SMALL, createCanvas } from 'common/setup';
-import { saveAsSVG, intersects, radiansToDegrees, timer } from 'common/utils';
-import math, { random } from 'mathjs';
-
-math.config({randomSeed: 101});
+import { saveAsSVG, intersects, radiansToDegrees, gauss } from 'common/utils';
+import math, { random, randomInt } from 'mathjs';
+import dat from 'dat.gui';
 
 const [width, height] = STRATH_SMALL.landscape;
 const canvas = createCanvas(STRATH_SMALL.landscape);
 
 paper.setup(canvas);
 window.paper = paper;
-
-const branches = [];
-const NODE_RADIUS = 10;
-const NODE_BRANCHES = 3;
-const DEPTH = 25;
 
 function Node (pos, direction, radius) {
   this.pos = pos;
@@ -28,83 +22,112 @@ function Branch (start, end) {
 }
 
 Branch.prototype.show = function () {
-  new Path.Line({
+  return new Path.Line({
     from: this.start.pos,
     to: this.end.pos,
     strokeColor: 'black'
   });
 }
 
-function drawOnce () {
-  const pointDist = 25;
-    // Does and entire tree before moving on to the next. needs to take a step for each tree.
-    const n1 = new Node(new Point(width/2 - pointDist, height/2 - pointDist), random2D(), NODE_RADIUS);
-    const n2 = new Node(new Point(width/2 + pointDist, height/2 - pointDist), random2D(), NODE_RADIUS);
-    const n3 = new Node(new Point(width/2 - pointDist, height/2 + pointDist), random2D(), NODE_RADIUS);
-    const n4 = new Node(new Point(width/2 + pointDist, height/2 + pointDist), random2D(), NODE_RADIUS);
+function placedRoots () {
+  const props = {
+    seed: 0,
+    radiusMin: 10,
+    radiusMax: 10,
+    radiusDistribution: 'uniform',
+    branchesMin: 3,
+    branchesMax: 3,
+    rotationMin: -0.5,
+    rotationMax: 0.5,
+    branchesDistribution: 'uniform',
+    roots: 4,
+    depth: 15,
+    boundaryRadius: 300,
+    run
+  };
 
+  const gui = new dat.GUI();
+  gui.add(props, 'seed');
+  gui.add(props, 'radiusMin');
+  gui.add(props, 'radiusMax');
+  gui.add(props, 'branchesMin').step(1);
+  gui.add(props, 'branchesMax').step(1);
+  gui.add(props, 'rotationMin');
+  gui.add(props, 'rotationMax');
+  gui.add(props, 'depth');
+  gui.add(props, 'boundaryRadius');
+  gui.add(props, 'run');
+
+  let group = new Group();
+
+  function run () {
+    group.remove();
+
+    if (props.seed) {
+      math.config({randomSeed: props.seed});
+    }
+
+    // const fns = {
+    //   getRadius: () => props.nodeRadius,
+    //   getNumBranches: () => props.nodeBranches
+    // };
+
+    const fns = {
+      getRadius: () => random(props.radiusMin, props.radiusMax),
+      getNumBranches: () => randomInt(props.branchesMin, props.branchesMax),
+      withinBoundary: (point) => {
+        const center = new Point(width / 2, height / 2);
+        return center.getDistance(point) < props.boundaryRadius;
+      },
+      rotation: () => {
+        return radiansToDegrees(random(props.rotationMin, props.rotationMax));
+      }
+    };
+
+    const roots = spacedRoots(25, fns.getRadius);
     let done = false;
-    const tree1 = treeGen(n1, DEPTH);
-    const tree2 = treeGen(n2, DEPTH);
-    const tree3 = treeGen(n3, DEPTH);
-    const tree4 = treeGen(n4, DEPTH);
+    group = new Group();
+    const branches = [];
+    const trees = roots.map(root => treeGen(root, props.depth, branches, fns, group));
     while (!done) {
-      const y1 = tree1.next();
-      const y2 = tree2.next();
-      const y3 = tree3.next();
-      const y4 = tree4.next();
-      done = (y1.done && y2.done && y3.done && y4.done);
+      const nexts = trees.map(tree => tree.next());
+      done = nexts.every(({done}) => done);
     }
     console.log("Done");
+  }
+
+  run();
+}
+
+function randomRoots () {
+  for (let i = 0; i < props.roots; i++) {
+    const n1 = new Node(new Point(width/2 - pointDist, height/2 - pointDist), random2D(), fns.getRadius());
+  }
+}
+
+function spacedRoots (pointDist = 25, getRadius) {
+  return [
+    new Node(new Point(width/2 - pointDist, height/2 - pointDist), random2D(), getRadius()),
+    new Node(new Point(width/2 + pointDist, height/2 - pointDist), random2D(), getRadius()),
+    new Node(new Point(width/2 - pointDist, height/2 + pointDist), random2D(), getRadius()),
+    new Node(new Point(width/2 + pointDist, height/2 + pointDist), random2D(), getRadius())
+  ];
 }
 
 function random2D () {
   return new Point(random(-1, 1), random(-1, 1)).normalize();
 }
 
-/**
- * @param {Number} radius
- * @param {Vector} direction
- */
-function getRadius (radius, direction) {
-  return NODE_RADIUS;
-
-  // return p5.random(5, 20);
-
-  // if (radius < 3) {
-  //   return radius;
-  // } else {
-  //   return radius / 1.3;
-  // }
-}
-
-function getNumBranches () {
-  // Constant
-  return NODE_BRANCHES;
-
-  // Random Number
-  // return Math.floor(p5.random(1, 4));
-
-  // Other options.
-  // 1. weighted random.
-  // 2. Probability dist.
-}
-
-function rotation () {
-  return radiansToDegrees(random(-0.5, 0.5));
-  // return p5.random(-1, 1);
-
-  // Other options
-  // 1. Use a flow field.
-}
-
-function* treeGen (node, depth) {
+function* treeGen (node, depth, branches, fns, group) {
+  const {getRadius, getNumBranches, withinBoundary, rotation} = fns;
   const layer = [];
   const nbranches = getNumBranches();
   for (let i = 0; i < nbranches; i++) {
-    const b = branch(node, branches);
+    const b = branch(node, branches, getRadius, rotation);
     if (b) {
-      b.show();
+      if (group) {
+        group.addChild(b.show());
+      }
       layer.push(b);
       branches.push(b);
     }
@@ -115,13 +138,13 @@ function* treeGen (node, depth) {
     for (let i = 0; i < layer.length; i++) {
       const branch = layer[i];
       if (withinBoundary(branch.end.pos)) {
-        yield* treeGen(branch.end, depth-1);
+        yield* treeGen(branch.end, depth-1, branches, fns, group);
       }
     }
   }
 }
 
-function branch (node, branches) {
+function branch (node, branches, getRadius, rotation) {
   // Randomly adjust the direction slightly.
   // let direction = Vector.fromAngle(node.direction.heading() + rotation());
   let direction = new Point({
@@ -141,24 +164,8 @@ function branch (node, branches) {
   }
 }
 
-const boundaryRadius = 300;
+placedRoots();
 
-function withinBoundary (point) {
-  // const center = p5.createVector(p5.width/2, p5.height/2);
-  // return p5.dist(p5.width/2, p5.height/2, point.x, point.y) < boundaryRadius;
-  const center = new Point(width / 2, height / 2);
-  return center.getDistance(point) < boundaryRadius;
-}
-
-// function drawBoundary () {
-//   p5.stroke(0);
-//   p5.ellipse(p5.width/2, p5.height/2, boundaryRadius * 2, boundaryRadius * 2);
-// }
-
-drawOnce();
-
-// Time without lzstring: 92502
-// Time with lzstring.compress: 89443 Not loading in Inkscape!
 window.saveAsSVG = (name) => {
   timer(() => saveAsSVG(paper.project, name));
 }
