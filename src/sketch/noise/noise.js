2beb257db893537e2e9b64cf3ac0be8266adb2ad
Sean Mullen <srmullen13@gmail.com>
2019-10-20 12:41:10 -0600

    value noise and smoother interpolation

--- src/sketch/noise/noise.js
+++ src/sketch/noise/noise.js
@@ -45,12 +45,119 @@ function dot2([a, b], [c, d]) {
   return a * c + b * d;
 }
 
+// Linear interpolation
 function lerp(from, to, val) {
   return from + val * (to - from);
 }
 
-class Perlin {
-  constructor () {
+// Cosine interpolation
+function cerp(from, to, val) {
+  return lerp(from, to, -Math.cos(Math.PI * val) / 2 + 0.5);
+}
+
+// Smooth step interpolation
+function smoothStep(from, to, val) {
+  return lerp(from, to, val * val * (3 - 2 * val));
+}
+
+// TODO:
+// Implement 3d perlin noise.
+// Create perlin noise on GPU.
+// Animate 3d perlin noise by traveling through z vec.
+// Marching cubes on noise.
+// Implement other noise types. Brownian, worley, curl perlin, curl worley.
+
+/**
+ * Essay Outline
+ * -------------
+ * - Implement ValueNoise2d on cpu.
+ * - Implement PerlinNoise2d on cpu.
+ * - Discuss interpolation.
+ * - Extend perlin to 3d.
+ * - Discuss performance enhancements. bitwise. flat grid.
+ * - Move to GPU.
+ * - Benchmarks.
+ */
+
+class ValueNoise2dGrid {
+  constructor() {
+    this.gridSize = 16;
+    this.grid = [];
+    // Create a grid of random values.
+    for (let x = 0; x < this.gridSize + 1; x++) {
+      const col = [];
+      for (let y = 0; y < this.gridSize + 1; y++) {
+        col.push(Math.random());
+      }
+      this.grid.push(col);
+    }
+  }
+
+  noise(x, y) {
+    const xpix = Math.floor(x) % this.gridSize;
+    const ypix = Math.floor(y) % this.gridSize;
+    const xoff = x - Math.floor(x);
+    const yoff = y - Math.floor(y);
+    const topLeft = this.grid[xpix][ypix];
+    const topRight = this.grid[xpix + 1][ypix];
+    const bottomRight = this.grid[xpix + 1][ypix + 1]
+    const bottomLeft = this.grid[xpix][ypix + 1];
+    const ab = lerp(topLeft, topRight, xoff);
+    const cd = lerp(bottomLeft, bottomRight, xoff);
+    return lerp(ab, cd, yoff);
+  }
+}
+
+class ValueNoise2d {
+  constructor() {
+    this.gridSize = 16;
+    this.grid = [];
+    // Create an array of random values.
+    for (let i = 0; i < Math.pow(this.gridSize, 2); i++) {
+      this.grid.push(Math.random());
+    }
+  }
+
+  getValue(x, y) {
+    return this.grid[y * this.gridSize + x];
+  }
+
+  noise(x, y) {
+    const xpix = Math.floor(x) % this.gridSize;
+    const ypix = Math.floor(y) % this.gridSize;
+    const xoff = x - Math.floor(x);
+    const yoff = y - Math.floor(y);
+    const topLeft = this.getValue(xpix, ypix);
+    const topRight = this.getValue(xpix + 1, ypix);
+    const bottomRight = this.getValue(xpix + 1, ypix + 1);
+    const bottomLeft = this.getValue(xpix, ypix + 1);
+
+    // const interpolate = lerp;
+    // const interpolate = cerp;
+    const interpolate = smoothStep;
+    const ab = interpolate(topLeft, topRight, xoff);
+    const cd = interpolate(bottomLeft, bottomRight, xoff);
+    return interpolate(ab, cd, yoff);
+  }
+}
+
+class ValueNoise3d {
+  constructor() {
+    this.gridSize = 16;
+    this.grid = [];
+    // Create a grid of random values.
+    for (let x = 0; x < this.gridSize + 1; x++) {
+      const col = [];
+      for (let y = 0; y < this.gridSize + 1; y++) {
+        col.push(Math.random());
+      }
+      this.grid.push(col);
+    }
+  }
+}
+
+class PerlinNoise2d {
+  constructor() {
     // Create the grid with random gradients.
     // There are other methods for initializing gradients. Such as using predefined.
     this.gridSize = 16;
@@ -62,19 +169,13 @@ class Perlin {
       }
       this.grid.push(col);
     }
-    this.gradients = [
-      [1, 1],
-      [-1, 1],
-      [-1, -1],
-      [1, -1]
-    ];
   }
 
   noise (x, y) {
     const xpix = Math.floor(x) % this.gridSize;
     const ypix = Math.floor(y) % this.gridSize;
-    const xoff = x - xpix;
-    const yoff = y - ypix;
+    const xoff = x - Math.floor(x);
+    const yoff = y - Math.floor(y);
     const gradients = [
       this.grid[xpix][ypix], // top left
       this.grid[xpix+1][ypix], // top right
@@ -97,23 +198,27 @@ class Perlin {
     for (let i = 0; i < 4; i++) {
       dots.push(dot2(gradients[i], vecs[i]));
     }
-    const ab = lerp(dots[0], dots[1], xoff);
-    const cd = lerp(dots[3], dots[2], xoff);
-    return lerp(ab, cd, yoff);
+
+    // const interpolate = lerp;
+    // const interpolate = cerp;
+    const interpolate = smoothStep;
+    const ab = interpolate(dots[0], dots[1], xoff);
+    const cd = interpolate(dots[3], dots[2], xoff);
+    return interpolate(ab, cd, yoff);
   }
 }
 
-const perlin = new Perlin();
+// const noise = new ValueNoise2d();
+const noise = new PerlinNoise2d();
 const noiseArr = [];
-for (let i = 0; i < 500; i++) {
+for (let i = 0; i < 750; i++) {
   const col = [];
-  for (let j = 0; j < 500; j++) {
-    col.push(perlin.noise(i * 0.01, j * 0.01));
+  for (let j = 0; j < 750; j++) {
+    // col.push(value.noise(i * 0.01, j * 0.01));
+    col.push(noise.noise(i * 0.01, j * 0.01));
   }
   noiseArr.push(col);
 }
-
-console.log(noiseArr);
 
 const gpu = new GPU({
   canvas,
@@ -133,7 +238,7 @@ const render = gpu.createKernel(function () {
   )
 }, {
   graphical: true,
-  output: [500, 500]
+  output: [750, 750]
 });
 
 // render();
@@ -145,10 +250,6 @@ const renderNoise = gpu.createKernel(function (noise) {
   this.color(v, v, v, 1);
 }, {
   graphical: true,
-  output: [500, 500]
+  output: [750, 750]
 });
 renderNoise(noiseArr);
-
-// TODO:
-// Read about value noise and implement.
-// Watch perlin noise video.
